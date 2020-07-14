4e75795852952046777f6bd4e23f6625fbd93fab
w3stling <peter.westling@gmail.com>
2020-07-14 21:50:49 +0200

    Updated test case

--- src/test/java/com/apptastic/mic/MicLookupTest.java
+++ src/test/java/com/apptastic/mic/MicLookupTest.java
@@ -71,7 +71,7 @@ public class MicLookupTest {
     public void testDownloadGetByCountryCode() throws IOException {
         MicLookup lookup = MicLookup.getInstance(true);
         assertTrue(lookup.isDownloaded());
-        assertEquals(39, lookup.getMicByCountryCode("SE").count());
+        assertEquals(40, lookup.getMicByCountryCode("SE").count());
         assertEquals(0, lookup.getMicByCountryCode("AA").count());
     }
 
