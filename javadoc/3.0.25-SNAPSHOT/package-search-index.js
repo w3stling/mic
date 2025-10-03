34e320c12ae435c8931387fadc11766caa5e3ed3
w3stling <w3stling@users.noreply.github.com>
2025-10-03 19:46:24 +0000

    Publishing javadoc

--- javadoc/3.0.25-SNAPSHOT/package-search-index.js
+++ javadoc/3.0.25-SNAPSHOT/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.apptasticsoftware.mic","l":"com.apptasticsoftware.mic"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"com.apptasticsoftware.mic","l":"com.apptasticsoftware.mic"}];updateSearchResults();
--- javadoc/3.0.25-SNAPSHOT/type-search-index.js
+++ javadoc/3.0.25-SNAPSHOT/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.apptasticsoftware.mic","l":"Mic"},{"p":"com.apptasticsoftware.mic","l":"MicLookup"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"com.apptasticsoftware.mic","l":"Mic"},{"p":"com.apptasticsoftware.mic","l":"MicLookup"}];updateSearchResults();
