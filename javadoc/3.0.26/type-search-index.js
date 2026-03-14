9296f432619d2544ddd8c707877f89584203e9dc
w3stling <w3stling@users.noreply.github.com>
2026-03-14 06:03:27 +0000

    Publishing javadoc

---
+++ javadoc/3.0.26/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"com.apptasticsoftware.mic","l":"Mic"},{"p":"com.apptasticsoftware.mic","l":"MicLookup"}];updateSearchResults();
